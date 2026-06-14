a0cf2fcee12061220f6676618154c07e517a8447
DieAlex01 <hannah.weichhart@gmail.com>
2026-06-14 14:31:51 +0200

    Input

--- Assignment4/app.R
+++ Assignment4/app.R
@@ -5,6 +5,8 @@ library(tidyverse)
 library(DT)
 #install.packages('rjson')
 library(rjson)
+#install.packages("countrycode")
+library("countrycode")
 
 data <- fromJSON(file = 'data_cia2.json')
 df <- tibble()
@@ -12,6 +14,10 @@ for (i in 1:length(data)){
   df = bind_rows(df, data[[i]])}
 colnames(df)
 variables <- list("Expenditure on education"= 'expenditure', "Youth unemployment rate" = 'youth_unempl_rate', "Net migration rate" = 'net_migr_rate', "Electricity from fossil fuels" = 'electricity_fossil_fuel', "Area"= 'area', "Population growth rate" = 'pop_growth_rate', "Life expectancy at birth"= 'life_expectancy')
+world_map<-map_data("world")
+world_map$ISO3<-countrycode::countrycode(sourcevar=world_map$region,
+                                         origin="country.name",
+                                         destination = "iso3c", nomatch = NA)
 
 # ── UI ────────────────────────────────────────────────────────────────────────
 
@@ -31,7 +37,7 @@ ui <- fluidPage(
           br(),
           selectInput('variable', 'Select a variable:', choices = variables, selected = "Expenditure on education"),
           actionButton('view', 'View raw data'),
-          DTOutput('raw')
+          DTOutput('raw', width = '80%')
         ),
         
         tabPanel(
@@ -76,9 +82,11 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   output$subtitle <- renderText({'This is a shiny app to visualize the variables from the CIA 2020 factbook!'})
-  eventReactive(input$raw, {
-    output$raw <- DT::renderDT(df[c('country', 'continent', input$variable)], otions = list(pageLength = 15))
+  observeEvent(input$view, {
+    output$raw <- DT::renderDT(df[c('country', 'continent', input$variable)], options = list(pageLength = 15))
   })
+  output$Map <- renderPlot({ggplot(world_map, aes(x=long,y=lat,group=group))+
+    geom_polygon(colour="white")})
   
   
 } # end server
