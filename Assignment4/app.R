fb401126480ede276fc81ef75d40b4c0d8b52cb6
DieAlex01 <hannah.weichhart@gmail.com>
2026-06-15 14:54:05 +0200

    layout update

--- Assignment4/app.R
+++ Assignment4/app.R
@@ -18,7 +18,7 @@ world_map<-map_data("world")
 world_map$ISO3<-countrycode::countrycode(sourcevar=world_map$region,
                                          origin="country.name",
                                          destination = "iso3c", nomatch = NA)
-
+df_world <- left_join(df, world_map, by = 'ISO3')
 # ── UI ────────────────────────────────────────────────────────────────────────
 
 ui <- fluidPage(
@@ -27,65 +27,64 @@ ui <- fluidPage(
   textOutput('subtitle'),
   br(),
   
-  sidebarLayout(
-    
-    # ── Sidebar: all inputs ──────────────────────────────────────────────────
-    sidebarPanel(
-      tabsetPanel(
-        tabPanel(
-          title = "Univariate analysis",
-          br(),
-          selectInput('variable', 'Select a variable:', choices = variables, selected = "Expenditure on education"),
-          actionButton('view', 'View raw data'),
-          DTOutput('raw', width = '80%')
-        ),
-        
-        tabPanel(
-          title = "Multivariate analysis",
-          br()
-        )
-      )
-    ), 
-    
+  mainPanel(
+    tabsetPanel(
+      tabPanel(title = 'Univariate Analysis',
+               sidebarLayout(
+                 
+                 # ── Sidebar: all inputs ──────────────────────────────────────────────────
+                 sidebarPanel(
+                   selectInput('variable', 'Select a variable:', choices = variables, selected = "Expenditure on education"),
+                   actionButton('view', 'View raw data'),
+                   DTOutput('raw'),
+                   width = 5),
+                     
+                     mainPanel(
+                       tabsetPanel(
+                         tabPanel(
+                           title = "Map",
+                           br(),
+                           plotOutput("Map")),
+                         
+                         tabPanel(
+                           title = "Global analysis",
+                           br(),
+                           DTOutput("table")),
+                         
+                         tabPanel(
+                           title = "Analysis per continent" ) 
+                      ),
+                      width = 7
+                    )
+                  )
+                ),
+      tabPanel(title = "Multivariate Analysis", 
+               sidebarLayout(
+                 sidebarPanel(
+               selectInput('variable_1', 'Select variable 1:', choices = variables, selected = "Expenditure on education"),
+               selectInput('variable_2', 'Select variable 2:', choices = variables, selected = "Youth unemployment rate"),
+               selectInput('variable_scale', 'Scale points by:', choices = variables, selected = "Area")
+              ),
+              mainPanel(plotOutput("scatter"))))
+              
+    ),width = 12))
+
     
     # ── Main panel: all outputs ──────────────────────────────────────────────
-    mainPanel(
-      
-      tabsetPanel(
-        
-        tabPanel(
-          title = "Map",
-          br(),
-          plotOutput("Map")        
-        ),
-        
-        tabPanel(
-          title = "Global analysis",
-          br(),
-          DTOutput("table")         
-        ),
-        
-        tabPanel(
-          title = "Analysis per continent"
-        )
-        
-      ) 
-      
-    ) 
-    
-  ) 
-  
-) 
+
 
 
 # ── Server ────────────────────────────────────────────────────────────────────
 
 server <- function(input, output, session) {
+  selected_var <- reactive({
+    return(input$variable)
+  })
   output$subtitle <- renderText({'This is a shiny app to visualize the variables from the CIA 2020 factbook!'})
   observeEvent(input$view, {
-    output$raw <- DT::renderDT(df[c('country', 'continent', input$variable)], options = list(pageLength = 15))
+    output$raw <- DT::renderDT(df_world[c('country', 'continent', input$variable)], options = list(pageLength = 15))
   })
-  output$Map <- renderPlot({ggplot(world_map, aes(x=long,y=lat,group=group))+
+  output$Map <- renderPlot({ggplot(df_world, aes(x=long,y=lat, group=group, fill =  input$variable))+#.data[[selected_var()]]))+
     geom_polygon(colour="white")})
   
   
